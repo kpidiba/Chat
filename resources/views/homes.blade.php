cbebcec8a0788100f3b141f41486280d54888922
d-programmer <kpidibadavid1@gmail.com>
2021-07-03 22:08:11 +0000

    debut de la page de chat partie 1

--- resources/views/homes.blade.php
+++ resources/views/homes.blade.php
@@ -9,32 +9,19 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row" id="mca_app">
-
-            <section class="col-12" id="mca_side_bar">
-                <div class="chat_profile">
-                    <img src="user.png" alt="">
-                    <span>Profle</span>
-                </div>
-                <div class="chat_menu">
-                    <ul>
-                        
-                        <li ><i class="fa fa-user"></i></li>
-                        <li ><i class="fa fa-compass"></i></li>
-                        <li ><i class="fa fa-google"></i></li>
-                        <li class="active">
-                            <i class="fa fa-bell"></i>
-                            <span class="icon-button__badge">2</span>
-                        </li>
-                        <li ><i class="fa fa-envelope"></i></li>
-                    </ul>
-                </div>
-                <div class="chat_bar">
-                    <i class="fa fa-bars"></i>
-                </div>
-            </section>
+<div class="bod" >
+    <div class="wrapper">
+        <div class="search-input">
+            <input type="text" placeholder="Search Your Friend ">
+            <div class="autocom-box">
+                <li>Broter in arms</li>
+                <li>Don't want this</li>
+                <li>Push up</li>
+                <li>Make this</li>
+            </div>
+            <div class="icon"><i class="fa fa-search"></i></div>
         </div>
     </div>
-
+    akakakak
+</div>
 @endsection
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -16,6 +16,7 @@
         <link rel="shortcut icon" type="image/x-icon" href="icon.png">
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link href="{{ asset('css/head.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
         <!-- Styles -->
@@ -23,6 +24,33 @@
         @yield('script')
     </head>
     <body>
+        <div class="container-fluid">
+            <div class="row" id="mca_app">
+    
+                <section class="col-12" id="mca_side_bar">
+                    <div class="chat_profile">
+                        <img src="user.png" alt="">
+                        <span>Profile</span>
+                    </div>
+                    <div class="chat_menu">
+                        <ul>
+                            <li class="active">
+                                <i class="fa fa-user"></i>
+                                <span class="icon-button__badge">2</span>
+                            </li>
+                            <li ><i class="fa fa-bell"></i></li>
+                            <li ><i class="fa fa-users"></i></li>
+                            <li ><i class="fa fa-google"></i></li>
+                            
+                            <li ><i class="fa fa-envelope"></i></li>
+                        </ul>
+                    </div>
+                    <div class="chat_bar">
+                        <i class="fa fa-bars"></i>
+                    </div>
+                </section>
+            </div>
+        </div>    
     <div id="app">
         <main class="py-4">
             @yield('content')
