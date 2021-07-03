dd0b2bae2e8d9fb0d5adb4dc549953f7af882cf8
d-programmer <kpidibadavid1@gmail.com>
2021-07-03 16:31:12 +0000

    fininsh notificatio ui

--- resources/views/homes.blade.php
+++ resources/views/homes.blade.php
@@ -14,15 +14,19 @@
 
             <section class="col-12" id="mca_side_bar">
                 <div class="chat_profile">
-                    <img src="icon.png" alt="">
+                    <img src="user.png" alt="">
                     <span>Profle</span>
                 </div>
                 <div class="chat_menu">
                     <ul>
-                        <li class="active"><i class="fa fa-bell"></i></li>
-                        <li ><i class="fa fa-file-text"></i></li>
+                        
+                        <li ><i class="fa fa-user"></i></li>
                         <li ><i class="fa fa-compass"></i></li>
                         <li ><i class="fa fa-google"></i></li>
+                        <li class="active">
+                            <i class="fa fa-bell"></i>
+                            <span class="icon-button__badge">2</span>
+                        </li>
                         <li ><i class="fa fa-envelope"></i></li>
                     </ul>
                 </div>
