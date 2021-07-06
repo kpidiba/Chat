6167e83fe57ce4322cc3bbd3c2dc7422e34f09ab
d-programmer <kpidibadavid1@gmail.com>
2021-07-06 00:07:19 +0000

    friend pop page-01

--- public/js/home.js
+++ public/js/home.js
@@ -5,3 +5,11 @@ document.getElementById("button").addEventListener("click",function(){
 document.querySelector(".close").addEventListener("click",function(){
     document.querySelector(".popup").style.display="none";
 })
+
+document.querySelector(".chat_profile").addEventListener("mouseover",function(){
+    document.querySelector(".menu").style.display="flex";
+})
+
+document.querySelector("body").addEventListener("click",function(){
+    document.querySelector(".menu").style.display="none";
+})
