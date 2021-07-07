4ace630453a06926bd68dd74678feabc03074bc5
d-programmer <kpidibadavid1@gmail.com>
2021-07-07 06:44:28 +0000

    migration pour la table Friend

---
+++ database/migrations/2021_07_07_064020_create_friends_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->increments('idFriend');
+            $table->integer('idUser')->unsigned();
+            $table->foreign('idUser')->references('idUser')->on('utilisateurs')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
