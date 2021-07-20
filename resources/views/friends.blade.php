af94c3cdff536699e62fcbaa4ffb3dc046902c41
d-programmer <kpidibadavid1@gmail.com>
2021-07-20 20:19:57 +0000

    debut de la page amis

--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -9,6 +9,33 @@
 @endsection
 
 @section('content')
-
+<div class="bod">
+    <div class="content">
+        <div class="tab_content">
+            <ul style="list-style: none;">
+                <li  data-li="prop" class="active inv-center" >Propositions</li>
+                <li  data-li="unread" class=" propo-center">Invitations</li>
+            </ul>
+        </div>
+        
+        <div class="inbox">
+            <ul style="list-style: none;">
+                <li>
+                    <div id="listp" class="zll prop">
+                        
+                    </div>
+                    
+                </li>
+                
+                <li>
+                    <div id="listv" class="zll unread">
+                        
+                    </div>
+                    
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
 <script src="{{ asset('js/home.js') }}" ></script>
 @endsection
